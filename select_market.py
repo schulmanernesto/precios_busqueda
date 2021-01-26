248f9a1b8f4872398900be13448d267242de8a05
Ernesto Schulman <ernestoschulman@gmail.com>
2021-01-25 23:50:25 -0300

    version 0.7 market

--- select_market.py
+++ select_market.py
@@ -2,30 +2,68 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
+import os, sys
+import datetime
+from natsort import index_natsorted
+import numpy as np
 import mercados
 
-def buscar(todos_los_productos):
-    """
-    docstring
-    """
-    busqueda = input("ingrese su busqueda: ")
-    if not busqueda == "":
-        todos_los_productos = []
-        driver.get(mercados.select_market+busqueda)
-        producto_a_objeto(todos_los_productos)
-        df = pd.DataFrame(todos_los_productos)
-        print(df)
-        busqueda = ""
-        todos_los_productos = []
-    buscar(todos_los_productos)
 
 def main():
     """
     docstring
     """
-    todos_los_productos = []
-    buscar(todos_los_productos)
 
+    categorias = ["ofertas", "frutas", "verduras", "organicos",
+                  "viandas", "limpieza", "perfumeria"]
+
+    almacen = ["fideos-y-legumbres", "snacks", "aceites-acetos", "galletitas-golosinas", "frutos-secos",
+               "especias", "salsas", "infusiones-endulzantes", "mermeladas-dulces", "conservas", "harinas", "aderezos"]
+    
+    frios = ["frescos", "congelados"]
+
+    bebidas = ["aguas","gaseosas","jugos","bebidas-sin-alcohol","cervezas","vinos"]
+
+    for i in range(len(categorias)):
+        buscar(categorias[i], categorias[i])
+
+    for i in range(len(almacen)):
+        buscar("almacen/"+almacen[i], almacen[i])
+
+    for i in range(len(frios)):
+        buscar("frios/"+frios[i], frios[i])    
+
+    for i in range(len(bebidas)):
+        buscar("bebidas/"+bebidas[i], bebidas[i])    
+
+def buscar(busqueda, categoria):
+    """
+    docstring
+    """
+    if not busqueda == "":
+        todos_los_productos = []
+        driver.get(mercados.select_market+busqueda)
+        producto_a_objeto(todos_los_productos)
+        df = pd.DataFrame(todos_los_productos)
+        orden = "producto"
+        ordenado = df.sort_values(
+            by=str(orden),
+            key=lambda x: np.argsort(index_natsorted(df[str(orden)]))
+        )
+        today = str(datetime.date.today())
+        localdir = os.getcwd()
+        #time = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-")
+        if not os.path.isdir(localdir+"/tablas/select_market/"+today):
+            os.mkdir(localdir+"/tablas/select_market/"+today)
+        
+        if not os.path.isfile(f"tablas/select_market/{today}/por_{orden}_{categoria}_tabla.csv"):
+            f = open(
+                f"tablas/select_market/{today}/por_{orden}_{categoria}_tabla.csv", "x")
+            f.write(ordenado.to_csv(index=False))
+            f.close()
+        busqueda = ""
+        orden = ""
+        print("busqueda finalizada")
 
 def producto_a_objeto(todos_los_productos):
 
@@ -38,16 +76,9 @@ def producto_a_objeto(todos_los_productos):
         todos_los_productos.append(
             {"producto": productos[i].text, "precio": precios[i].text.replace("\n", " ")})
 
-def is_int(string):  
-    final = ''  
-    for i in string:  
-        try:   
-            final += str(int(i))   
-        except ValueError:  
-            return int(final)
-
-
 if __name__ == "__main__":
+#   options = Options()
+#   options.headless = True
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
@@ -57,6 +88,3 @@ if __name__ == "__main__":
     driver.implicitly_wait(5)
     print("inicializacion finalizada.")
     main()
-
-
-
